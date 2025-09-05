7df4e18144dbe8cad40e3ac134bc1fd41d882f8b
nvila-iso <nvila88@proton.me>
2025-09-05 13:31:45 -0400

    completed.

--- app.js
+++ app.js
@@ -14,11 +14,12 @@ app.use("/playlists/:id/tracks", playlistsRouter);
 
 app.use((err, req, res, next) => {
   // Foreign key violation
-  if (err.code === "23503") {
-    return res.status(400).send(err.detail);
-  }
 
-  if (err.code === "22P02" || err.code === "22007" || err.code === "22008") {
+  if (
+    err.code === "22P02" || // invalid_text_rep
+    err.code === "23503" || // foreign key violation
+    err.code === "23505" // unique violation
+  ) {
     return res.status(400).send(err.detail);
   }
 
